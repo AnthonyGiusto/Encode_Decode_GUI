b7b3eca6b6eca7954769b0632bfd7f4661afa5a6
Anthony Giusto <giustoanthony@hotmail.com>
2020-01-04 23:00:51 +0100

    Add files via upload

---
+++ GUI.py
@@ -0,0 +1,78 @@
+import tkinter as tk
+from tkinter import *
+from encode import Encode
+from decode import Decode
+
+class EncodeGui:
+
+	def __init__(self, master):
+		self.master = master
+		master.title("Encode_GUI")
+		master.geometry("375x105")
+
+		#calls for a user entry for the index number
+		self.index = Entry(window, width = 5)
+		self.index.grid(column=7, row=3)
+
+		self.index_number1 = Label(window, text="Index")
+		self.index_number1.grid(column=5, row=3)
+		#creates a label for the Number entry
+
+		self.lbl_number1 = Label(window, text="Number")
+		self.lbl_number1.grid(column=1, row=1)
+		
+		#creates a label for the encode result
+		self.lbl_code = Label(window, text="Encoded")
+		self.lbl_code.grid(column =1, row = 3)
+
+		#creates a placeholder for the encode result
+		self.encode_blankentry = tk.Label(window, text="")
+		self.encode_blankentry.grid(column =2, row = 3)
+
+		#calls for a user entry for the number to be encoded
+		self.plain_txt = Entry(window, width = 15)
+		self.plain_txt.grid(column=2, row=1)
+
+		#creates a Encode button
+		self.btn_encode = Button(text="Encode", command=self.encode_click,fg = "white", bg = "black")
+		self.btn_encode.grid(column=3, row=1)
+
+		#creates a label for the Number entry
+		self.lbl_code = Label(window, text="Code")
+		self.lbl_code.grid(column=1, row=5)
+
+		#creates a label for the code result
+		self.lbl_number2 = Label(window, text="Decoded")
+		self.lbl_number2.grid(column =1, row = 6)
+
+
+		#calls for a user entry for the code to be decoded
+		self.code_entry = Entry(window, width = 15)
+		self.code_entry.grid(column=2, row=5)
+
+		#creates a Decode button
+		self.btn_decode = Button(text="Decode", command=self.decode_click,fg = "white", bg = "black")
+		self.btn_decode.grid(column=3, row=5)
+
+		#creates a placeholder for the decode result
+		self.decode_blankentry= tk.Label(window, text="")
+		self.decode_blankentry.grid(column =2, row = 6)
+	
+
+	def encode_click(self):
+		"""Takes a user input value and encodes it with a predefined month index"""
+		index=int(self.index.get())
+		number=str(self.plain_txt.get())
+		encoded = Encode.encode(number,index)
+		self.encode_blankentry.configure(text= encoded)
+	
+	def decode_click(self):
+		index=int(self.index.get())
+		code=str(self.code_entry.get())
+		decoded = Decode.decode(code,index)
+		self.decode_blankentry.configure(text= decoded)
+
+
+window = Tk()
+encode_gui = EncodeGui(window)
+window.mainloop()
---
+++ encode.py
@@ -0,0 +1,28 @@
+class Encode:
+
+	def __init__(self):
+		"""initializes the encode attributes."""
+		self = self
+
+	def encode(number,month):
+		"""Takes a user input value and encodes it with a predefined month index"""
+
+		#creates known lists of indexes for encoding
+		nullcode = {1:'i',2:'c',3:'e',4:'d',5:'r',6:'a',7:'g',8:'o',9:'n',0:'s'}
+		code_one = {1:'n',2:'e',3:'a',4:'r',5:'d',6:'g',7:'s',8:'i',9:'o',0:'c'}
+		code_two = {1:'c',2:'s',3:'g',4:'r',5:'d',6:'i',7:'o',8:'a',9:'e',0:'n'}
+		code_three = {1:'a',2:'e',3:'r',4:'n',5:'s',6:'i',7:'o',8:'g',9:'c',0:'d'}
+		code_four = {1:'d',2:'r',3:'a',4:'s',5:'g',6:'o',7:'c',8:'i',9:'e',0:'n'}
+		codelist = [nullcode,code_one, code_two, code_three, code_four]
+
+		#User input for the number to be encoded
+
+		digitlist = []
+		encodedstring = []
+		codeindex = codelist[month].copy()
+		for digit in number:
+			digitlist.append(digit)
+		for digit in digitlist:
+			digit = int(digit)
+			encodedstring.append(codeindex[digit])
+		return encodedstring
